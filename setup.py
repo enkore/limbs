8b41002e84383d49006e4dc6a98102319bdc223c
enkore <public@enkore.de>
2015-05-26 16:05:34 +0200

    Remove py.test/setuptools integration completly
    It doesn't work and I'm way to annoyed by anything connected
    to setup* to investigate.

--- setup.py
+++ setup.py
@@ -3,35 +3,11 @@
 import sys
 
 from setuptools import setup
-from setuptools.command.test import test as TestCommand
 
-
-class PyTest(TestCommand):
-    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
-
-    def initialize_options(self):
-        TestCommand.initialize_options(self)
-        self.pytest_args = []
-
-    def finalize_options(self):
-        TestCommand.finalize_options(self)
-        self.test_args = []
-        self.test_suite = True
-
-    def run_tests(self):
-        # import here, cause outside the eggs aren't loaded
-        import pytest
-        errno = pytest.main(self.pytest_args)
-        sys.exit(errno)
-
-setup(name="limbs", version="0.1",
+setup(name="limbs", version="0.2",
       description="like mime but simpler - a simple, human-readable format",
       url="http://github.com/enkore/limbs/",
       author="Marian Beermann", author_email="public@enkore.de",
       license="MIT",
       py_modules=["limbs"],
-
-      # py.test integration
-      tests_require=['pytest'],
-      cmdclass = {'test': PyTest},
       )
