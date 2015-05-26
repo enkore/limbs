0357c2710d1be9ac0c03f3c56abdac8c7bd3a81c
enkore <public@enkore.de>
2015-05-26 16:08:32 +0200

    Classifiers

--- setup.py
+++ setup.py
@@ -8,6 +8,12 @@ setup(name="limbs", version="0.2",
       description="like mime but simpler - a simple, human-readable format",
       url="http://github.com/enkore/limbs/",
       author="Marian Beermann", author_email="public@enkore.de",
-      license="MIT",
+      classifiers=[
+          "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+          "Intended Audience :: Developers",
+          "Development Status :: 4 - Beta",
+          "Programming Language :: Python :: 3 :: Only",
+          "Topic :: Software Development :: Libraries",
+      ],
       py_modules=["limbs"],
       )
